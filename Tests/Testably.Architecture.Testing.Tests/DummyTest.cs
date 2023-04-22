6b0a5e7bb469b53f9c8a5fb1c9856751ee9c9ca7
Valentin Breuß <vbreuss@gmail.com>
2023-04-22 09:52:46 +0200

    feat: add build pipelines (#1)
    Co-authored-by: Valentin Breuß <v.breuss@tig.at>

---
+++ Tests/Testably.Architecture.Testing.Tests/DummyTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Testably.Architecture.Testing.Tests;
+
+public class DummyTest
+{
+	[Fact]
+	public void Dummy()
+	{
+		//This is a dummy test!
+
+		bool result = true;
+
+		result.Should().BeTrue();
+	}
+}
