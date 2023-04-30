90230570086b8ea6da1f7489d791c6aa8cc9ed4e
Valentin Breuß <v.breuss@tig.at>
2023-04-30 15:57:47 +0200

    Add extensions for types

---
+++ Source/Testably.Architecture.Testing/Extensions.ITypeExpectation.cs
@@ -0,0 +1,14 @@
+﻿namespace Testably.Architecture.Testing;
+
+public static partial class Extensions
+{
+	/// <summary>
+	///     Expect the type to be sealed.
+	/// </summary>
+	public static ITestResult<ITypeExpectation> ShouldBeSealed(
+		this ITypeExpectation @this,
+		bool isSealed = true)
+	{
+		return @this.ShouldSatisfy(t => t.IsSealed == isSealed);
+	}
+}
---
+++ Source/Testably.Architecture.Testing/Extensions.MethodInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Testably.Architecture.Testing;
+
+/// <summary>
+///     Extension methods for <see cref="MethodInfo" />.
+/// </summary>
+public static partial class Extensions
+{
+	/// <summary>
+	///     Checks if the <paramref name="methodInfo" /> has an attribute which satisfies the <paramref name="predicate" />.
+	/// </summary>
+	/// <typeparam name="TAttribute">The type of the <see cref="Attribute" />.</typeparam>
+	/// <param name="methodInfo">The <see cref="MethodInfo" /> which is checked to have the attribute.</param>
+	/// <param name="predicate">
+	///     (optional) A predicate to check.
+	///     <para />
+	///     If not set (<see langword="null" />), will only check if the attribute is present.
+	/// </param>
+	public static bool HasAttribute<TAttribute>(
+		this MethodInfo methodInfo,
+		Func<TAttribute, bool>? predicate = null)
+	{
+		object? attribute = methodInfo.GetCustomAttributes(typeof(TAttribute), true)
+		   .FirstOrDefault();
+		if (attribute is TAttribute castedAttribute)
+		{
+			return predicate?.Invoke(castedAttribute) ?? true;
+		}
+
+		return false;
+	}
+}
