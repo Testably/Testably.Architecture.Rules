82f4e4abc7e2081e622b409ddbe9daebb5f1e4fb
Valentin Breuß <v.breuss@tig.at>
2023-05-06 22:57:21 +0200

    Add example tests project

---
+++ Tests/Testably.Architecture.Example.Tests/ExampleTests.cs
@@ -0,0 +1,25 @@
+﻿using Testably.Architecture.Example.Tests.TestHelpers;
+using Testably.Architecture.Rules;
+using Xunit;
+
+namespace Testably.Architecture.Example.Tests;
+
+public sealed class ExampleTests
+{
+	/// <summary>
+	///     All test classes should be sealed.
+	///     <para />
+	///     Enforces this rule in assembly `Testably.Architecture.Rules.Tests`
+	/// </summary>
+	[Fact]
+	public void ExpectTestClassesToBeSealed()
+	{
+		IRule rule = Expect.That.Types
+			.WhichAreTestClasses()
+			.ShouldBeSealed();
+
+		rule.Check
+			.InTestAssembly()
+			.ThrowIfViolated();
+	}
+}
