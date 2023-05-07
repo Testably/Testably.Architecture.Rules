0f061aac7adc48e57b0d1305038a646c827dd48f
Valentin Breuß <v.breuss@tig.at>
2023-05-07 20:34:11 +0200

    Add Tests for TestResultExtensions

---
+++ Source/Testably.Architecture.Rules/Internal/ThrowHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+
+namespace Testably.Architecture.Rules.Internal;
+
+internal static class ThrowHelper
+{
+	/// <summary>
+	///     Throw an exception containing the <paramref name="message" />.
+	/// </summary>
+	public static void Throw(string message)
+	{
+		ThrowIfViolatedUsingXunit(message);
+		throw new ArchitectureRuleViolatedException(message);
+	}
+
+	/// <summary>
+	///     Try to throw the <paramref name="message" /> as a `XunitException`
+	///     to improve readability in the test result window.
+	/// </summary>
+	/// <param name="message"></param>
+	[ExcludeFromCodeCoverage]
+	private static void ThrowIfViolatedUsingXunit(string message)
+	{
+		try
+		{
+			Type? exceptionType = Assembly
+				.Load(new AssemblyName("xunit.assert"))
+				.GetType("Xunit.Sdk.XunitException");
+			if (exceptionType != null &&
+			    Activator.CreateInstance(exceptionType, message) is Exception
+				    exception)
+			{
+				throw exception;
+			}
+		}
+		catch (FileNotFoundException)
+		{
+			// xunit.assert could not be loaded.
+		}
+	}
+}
