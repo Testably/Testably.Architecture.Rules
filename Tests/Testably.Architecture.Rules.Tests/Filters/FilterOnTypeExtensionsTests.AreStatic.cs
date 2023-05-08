7643d390bdd56d2eecd390e356f2bfea11b56362
Valentin Breuß <v.breuss@tig.at>
2023-05-08 20:53:55 +0200

    Add tests

---
+++ Tests/Testably.Architecture.Rules.Tests/Filters/FilterOnTypeExtensionsTests.AreStatic.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Testably.Architecture.Rules.Tests.TestHelpers;
+using Xunit;
+
+namespace Testably.Architecture.Rules.Tests.Filters;
+
+public sealed partial class FilterOnTypeExtensionsTests
+{
+	public sealed class AreStaticTests
+	{
+		[Fact]
+		public void WhichAreNotStatic_ShouldFilterForNotStaticTypes()
+		{
+			ITestResult result = Expect.That.Types
+				.WhichAre(typeof(StaticClass), typeof(NotStaticClass)).And
+				.WhichAreNotStatic()
+				.ShouldAlwaysFail()
+				.Check.InAllLoadedAssemblies();
+
+			result.Errors.Length.Should().Be(1);
+			result.Errors[0].ToString().Should()
+				.Contain(typeof(NotStaticClass).FullName);
+		}
+
+		[Fact]
+		public void WhichAreStatic_ShouldFilterForStaticTypes()
+		{
+			ITestResult result = Expect.That.Types
+				.WhichAre(typeof(StaticClass), typeof(NotStaticClass)).And
+				.WhichAreStatic()
+				.ShouldAlwaysFail()
+				.Check.InAllLoadedAssemblies();
+
+			result.Errors.Length.Should().Be(1);
+			result.Errors[0].ToString().Should()
+				.Contain(typeof(StaticClass).FullName);
+		}
+
+		private class NotStaticClass
+		{
+		}
+
+		private static class StaticClass
+		{
+		}
+	}
+}
