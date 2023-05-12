3d186203088edbe2e79cd79561b6bbd1c07f5b97
Valentin Breuß <vbreuss@gmail.com>
2023-05-12 20:36:40 +0200

    feat: add additional filter extensions (#42)
    Add additional filter extensions:
    - WhichAreAbstract
    - WhichAreAClass
    - WhichAreAnInterface
    - WhichAreGeneric
    - WhichAreNested
    - WhichArePrivate
    - WhichAreSealed
    - WhichAreStatic
    - WhichInheritFrom
    - WhichMatchName
    
    ---------
    
    Co-authored-by: Valentin Breuß <v.breuss@tig.at>

---
+++ Tests/Testably.Architecture.Rules.Tests/Filters/FilterOnTypeExtensionsTests.AreNested.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Testably.Architecture.Rules.Tests.TestHelpers;
+using Xunit;
+
+namespace Testably.Architecture.Rules.Tests.Filters;
+
+public sealed partial class FilterOnTypeExtensionsTests
+{
+	public sealed class AreNestedTests
+	{
+		[Fact]
+		public void WhichAreNested_ShouldFilterForNestedTypes()
+		{
+			ITestResult result = Expect.That.Types
+				.WhichAre(typeof(NestedClass), typeof(FilterOnTypeExtensionsTests)).And
+				.WhichAreNested()
+				.ShouldAlwaysFail()
+				.Check.InAllLoadedAssemblies();
+
+			result.Errors.Length.Should().Be(1);
+			result.Errors[0].ToString().Should()
+				.Contain(typeof(NestedClass).FullName);
+		}
+
+		[Fact]
+		public void WhichAreNotNested_ShouldFilterForNotNestedTypes()
+		{
+			ITestResult result = Expect.That.Types
+				.WhichAre(typeof(NestedClass), typeof(FilterOnTypeExtensionsTests)).And
+				.WhichAreNotNested()
+				.ShouldAlwaysFail()
+				.Check.InAllLoadedAssemblies();
+
+			result.Errors.Length.Should().Be(1);
+			result.Errors[0].ToString().Should()
+				.Contain(typeof(FilterOnTypeExtensionsTests).FullName);
+		}
+
+		private abstract class NestedClass
+		{
+		}
+	}
+}
