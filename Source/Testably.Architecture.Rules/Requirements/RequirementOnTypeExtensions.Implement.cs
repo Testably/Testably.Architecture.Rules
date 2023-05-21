1e70dec047ef5dac277bf9168338ebd2499abb49
Valentin Breuß <v.breuss@tig.at>
2023-05-21 14:46:01 +0200

    Add Implement Requirements

---
+++ Source/Testably.Architecture.Rules/Requirements/RequirementOnTypeExtensions.Implement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Testably.Architecture.Rules;
+
+public static partial class RequirementOnTypeExtensions
+{
+	/// <summary>
+	///     Expect the types to implement the <typeparamref name="TInterface" />.
+	/// </summary>
+	/// <typeparamref name="TInterface">The type of the interface which should be implemented.</typeparamref>
+	/// <param name="this">The <see cref="IRequirement{Type}" />.</param>
+	/// <param name="forceDirect">
+	///     If set to <see langword="false" /> (default value), the <typeparamref name="TInterface" />
+	///     can be anywhere in the inheritance tree, otherwise if set to <see langword="true" /> requires the
+	///     <typeparamref name="TInterface" /> to be directly implemented.
+	/// </param>
+	public static IRequirementResult<Type> ShouldImplement<TInterface>(
+		this IRequirement<Type> @this,
+		bool forceDirect = false)
+		=> @this.ShouldImplement(typeof(TInterface), forceDirect);
+
+	/// <summary>
+	///     Expect the types to implement the <paramref name="interfaceType" />.
+	/// </summary>
+	/// <param name="this">The <see cref="IRequirement{Type}" />.</param>
+	/// <param name="interfaceType">The interface which should be implemented.</param>
+	/// <param name="forceDirect">
+	///     If set to <see langword="false" /> (default value), the <paramref name="interfaceType" />
+	///     can be anywhere in the inheritance tree, otherwise if set to <see langword="true" /> requires the
+	///     <paramref name="interfaceType" /> to be directly implemented.
+	/// </param>
+	public static IRequirementResult<Type> ShouldImplement(
+		this IRequirement<Type> @this,
+		Type interfaceType,
+		bool forceDirect = false)
+		=> @this.ShouldSatisfy(Requirement.ForType(
+			type => type.Implements(interfaceType, forceDirect),
+			type => new TypeTestError(type,
+				$"Type '{type.Name}' should{(forceDirect ? " directly" : "")} implement '{interfaceType.Name}'.")));
+
+	/// <summary>
+	///     Expect the types to not implement the <typeparamref name="TInterface" />.
+	/// </summary>
+	/// <typeparamref name="TInterface">The type of the interface which should be implemented.</typeparamref>
+	/// <param name="this">The <see cref="IRequirement{Type}" />.</param>
+	/// <param name="forceDirect">
+	///     If set to <see langword="false" /> (default value), the <typeparamref name="TInterface" />
+	///     can be anywhere in the inheritance tree, otherwise if set to <see langword="true" /> requires the
+	///     <typeparamref name="TInterface" /> to be directly implemented.
+	/// </param>
+	public static IRequirementResult<Type> ShouldNotImplement<TInterface>(
+		this IRequirement<Type> @this,
+		bool forceDirect = false)
+		=> @this.ShouldNotImplement(typeof(TInterface), forceDirect);
+
+	/// <summary>
+	///     Expect the types to not implement the <paramref name="interfaceType" />.
+	/// </summary>
+	/// <param name="this">The <see cref="IRequirement{Type}" />.</param>
+	/// <param name="interfaceType">The interface which should be implemented.</param>
+	/// <param name="forceDirect">
+	///     If set to <see langword="false" /> (default value), the <paramref name="interfaceType" />
+	///     can be anywhere in the inheritance tree, otherwise if set to <see langword="true" /> requires the
+	///     <paramref name="interfaceType" /> to be directly implemented.
+	/// </param>
+	public static IRequirementResult<Type> ShouldNotImplement(
+		this IRequirement<Type> @this,
+		Type interfaceType,
+		bool forceDirect = false)
+		=> @this.ShouldSatisfy(Requirement.ForType(
+			type => !type.Implements(interfaceType, forceDirect),
+			type => new TypeTestError(type,
+				$"Type '{type.Name}' should not{(forceDirect ? " directly" : "")} implement '{interfaceType.Name}'.")));
+}
---
+++ Tests/Testably.Architecture.Rules.Tests/Requirements/RequirementOnTypeExtensionsTests.Implement.cs
@@ -0,0 +1,220 @@
+﻿using FluentAssertions;
+using System;
+using Testably.Architecture.Rules.Tests.TestHelpers;
+using Xunit;
+
+namespace Testably.Architecture.Rules.Tests.Requirements;
+
+public sealed partial class RequirementOnTypeExtensionsTests
+{
+	public sealed class ImplementTests
+	{
+		[Theory]
+		[InlineData(false)]
+		[InlineData(true)]
+		public void
+			ShouldImplement_WithGenericInterface_ForceDirect_ShouldConsiderParameter(
+				bool forceDirect)
+		{
+			Type type = typeof(GenericFooImplementor2<>);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldImplement(typeof(IGenericFooInterface<>),
+					forceDirect: forceDirect);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolatedIf(forceDirect);
+		}
+
+		[Theory]
+		[InlineData(false)]
+		[InlineData(true)]
+		public void ShouldImplement_ForceDirect_WithInterface_ShouldConsiderParameter(
+			bool forceDirect)
+		{
+			Type type = typeof(FooImplementor2);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldImplement<IFooInterface>(
+					forceDirect: forceDirect);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolatedIf(forceDirect);
+		}
+
+		[Theory]
+		[InlineData(false)]
+		[InlineData(true)]
+		public void ShouldImplement_ToString_ShouldBeCorrect(bool forceDirect)
+		{
+			Type type = typeof(FooImplementor2);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldImplement<IOtherFooInterface>(
+					forceDirect: forceDirect);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolated();
+			result.Errors[0].Should().BeOfType<TypeTestError>()
+				.Which.Type.Should().Be(type);
+			result.Errors[0].ToString().Should().Contain($"Type '{type.Name}'")
+				.And.Contain(
+					$"should{(forceDirect ? " directly" : "")} implement '{nameof(IOtherFooInterface)}'.");
+		}
+
+		[Fact]
+		public void ShouldImplement_WithGenericClass_ShouldNotBeSatisfied()
+		{
+			Type type = typeof(GenericFooImplementor2<>);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldImplement(typeof(GenericFooImplementor1<>));
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolated();
+			result.Errors[0].Should().BeOfType<TypeTestError>()
+				.Which.Type.Should().Be(type);
+		}
+
+		[Fact]
+		public void ShouldImplement_WithClass_ShouldNotBeSatisfied()
+		{
+			Type type = typeof(FooImplementor2);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldImplement<FooImplementor1>();
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolated();
+			result.Errors[0].Should().BeOfType<TypeTestError>()
+				.Which.Type.Should().Be(type);
+		}
+
+		[Theory]
+		[InlineData(false)]
+		[InlineData(true)]
+		public void
+			ShouldNotImplement_WithGenericInterface_ForceDirect_ShouldConsiderParameter(
+				bool forceDirect)
+		{
+			Type type = typeof(GenericFooImplementor2<>);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldNotImplement(typeof(IGenericFooInterface<>),
+					forceDirect: forceDirect);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolatedIf(!forceDirect);
+		}
+
+		[Theory]
+		[InlineData(false)]
+		[InlineData(true)]
+		public void ShouldNotImplement_ForceDirect_WithInterface_ShouldConsiderParameter(
+			bool forceDirect)
+		{
+			Type type = typeof(FooImplementor2);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldNotImplement<IFooInterface>(
+					forceDirect: forceDirect);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolatedIf(!forceDirect);
+		}
+
+		[Theory]
+		[InlineData(false)]
+		[InlineData(true)]
+		public void ShouldNotImplement_ToString_ShouldBeCorrect(bool forceDirect)
+		{
+			Type type = typeof(FooImplementor2);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldNotImplement<IOtherFooInterface>(
+					forceDirect: forceDirect);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldNotBeViolated();
+		}
+
+		[Fact]
+		public void ShouldNotImplement_WithGenericClass_ShouldNotBeSatisfied()
+		{
+			Type type = typeof(GenericFooImplementor2<>);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldNotImplement(typeof(GenericFooImplementor1<>));
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldNotBeViolated();
+		}
+
+		[Fact]
+		public void ShouldNotImplement_WithClass_ShouldNotBeSatisfied()
+		{
+			Type type = typeof(FooImplementor2);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldNotImplement<FooImplementor1>();
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldNotBeViolated();
+		}
+
+		private class FooImplementor1 : IFooInterface
+		{
+		}
+
+		private class FooImplementor2 : FooImplementor1
+		{
+		}
+
+		private class GenericFooImplementor1<T> : IGenericFooInterface<T>,
+			IFooInterface
+		{
+		}
+
+		private class GenericFooImplementor2<T> : GenericFooImplementor1<T>
+		{
+		}
+
+		private interface IFooInterface
+		{
+		}
+
+		// ReSharper disable once UnusedTypeParameter
+		private interface IGenericFooInterface<T>
+		{
+		}
+
+		private interface IOtherFooInterface
+		{
+		}
+
+		// ReSharper disable once UnusedTypeParameter
+		private interface IOtherGenericFooInterface<T>
+		{
+		}
+	}
+}
