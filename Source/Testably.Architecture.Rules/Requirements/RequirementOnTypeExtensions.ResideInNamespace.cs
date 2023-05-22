a5a1a63c6c4f5b13fd68480d28db4bb7846eb465
Valentin Breuß <vbreuss@gmail.com>
2023-05-22 11:37:26 +0200

    feat: add `ResideInNamespace` extensions (#51)
    Add extension methods for `ResideInNamespace`
    
    ---------
    
    Co-authored-by: Valentin Breuß <v.breuss@tig.at>

---
+++ Source/Testably.Architecture.Rules/Requirements/RequirementOnTypeExtensions.ResideInNamespace.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Testably.Architecture.Rules;
+
+public static partial class RequirementOnTypeExtensions
+{
+	/// <summary>
+	///     Expect the <see cref="System.Type.Namespace" /> of the types to match the given <paramref name="pattern" />.
+	/// </summary>
+	/// <param name="this">The <see cref="IRequirement{Type}" />.</param>
+	/// <param name="pattern">
+	///     The wildcard condition.
+	///     <para />
+	///     Supports * to match zero or more characters and ? to match exactly one character.
+	/// </param>
+	/// <param name="ignoreCase">Flag indicating if the comparison should be case sensitive or not.</param>
+	public static IRequirementResult<Type> ShouldResideInNamespace(
+		this IRequirement<Type> @this,
+		Match pattern,
+		bool ignoreCase = false)
+		=> @this.ShouldSatisfy(Requirement.ForType(
+			type => pattern.Matches(type.Namespace, ignoreCase),
+			type => new TypeTestError(type,
+				$"The namespace of type '{type.Name}' should match pattern '{pattern}'.")));
+
+	/// <summary>
+	///     Expect the <see cref="System.Type.Namespace" /> of the types to not match the given <paramref name="pattern" />.
+	/// </summary>
+	/// <param name="this">The <see cref="IRequirement{Type}" />.</param>
+	/// <param name="pattern">
+	///     The wildcard condition.
+	///     <para />
+	///     Supports * to match zero or more characters and ? to match exactly one character.
+	/// </param>
+	/// <param name="ignoreCase">Flag indicating if the comparison should be case sensitive or not.</param>
+	public static IRequirementResult<Type> ShouldNotResideInNamespace(
+		this IRequirement<Type> @this,
+		Match pattern,
+		bool ignoreCase = false)
+		=> @this.ShouldSatisfy(Requirement.ForType(
+			type => !pattern.Matches(type.Namespace, ignoreCase),
+			type => new TypeTestError(type,
+				$"The namespace of type '{type.Name}' should not match pattern '{pattern}'.")));
+}
---
+++ Tests/Testably.Architecture.Rules.Tests/Requirements/RequirementOnTypeExtensionsTests.ResideInNamespace.cs
@@ -0,0 +1,126 @@
+﻿using FluentAssertions;
+using System;
+using Testably.Architecture.Rules.Tests.TestHelpers;
+using Xunit;
+
+namespace Testably.Architecture.Rules.Tests.Requirements;
+
+public sealed partial class RequirementOnTypeExtensionsTests
+{
+	public sealed class ResideInNamespaceTests
+	{
+		[Theory]
+		[InlineData(false)]
+		[InlineData(true)]
+		public void ShouldResideInNamespace_IgnoreCase_ShouldConsiderParameter(bool ignoreCase)
+		{
+			Type type = typeof(ResideInNamespaceTests);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldResideInNamespace("TESTABLY.architecture.RULES.tests.REQUIREMENTS",
+					ignoreCase);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolatedIf(!ignoreCase);
+		}
+
+		[Theory]
+		[InlineData("*")]
+		[InlineData("Test????.Architecture.Rules.Tests.Requirements")]
+		[InlineData("Testably.Architecture.Rules.Tests.Requirements")]
+		public void ShouldResideInNamespace_MatchingPattern_ShouldNotBeViolated(
+			string matchingPattern)
+		{
+			Type type = typeof(ResideInNamespaceTests);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldResideInNamespace(matchingPattern);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldNotBeViolated();
+		}
+
+		[Theory]
+		[InlineData("*Foo")]
+		[InlineData("Test??.Architecture.Rules.Tests.Requirements")]
+		[InlineData("TESTABLY.architecture.RULES.tests.REQUIREMENTS")]
+		public void ShouldResideInNamespace_NotMatchingPattern_ShouldNotBeSatisfied(
+			string notMatchingPattern)
+		{
+			Type type = typeof(ResideInNamespaceTests);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldResideInNamespace(notMatchingPattern);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolated();
+			result.Errors[0].Should().BeOfType<TypeTestError>()
+				.Which.Type.Should().Be(type);
+			result.Errors[0].Should().BeOfType<TypeTestError>()
+				.Which.ToString().Should().Contain($"'{notMatchingPattern}'");
+		}
+
+		[Theory]
+		[InlineData(false)]
+		[InlineData(true)]
+		public void ShouldNotResideInNamespace_IgnoreCase_ShouldConsiderParameter(bool ignoreCase)
+		{
+			Type type = typeof(ResideInNamespaceTests);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldNotResideInNamespace("TESTABLY.architecture.RULES.tests.REQUIREMENTS",
+					ignoreCase);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolatedIf(ignoreCase);
+		}
+
+		[Theory]
+		[InlineData("*")]
+		[InlineData("Test????.Architecture.Rules.Tests.Requirements")]
+		[InlineData("Testably.Architecture.Rules.Tests.Requirements")]
+		public void ShouldNotResideInNamespace_MatchingPattern_ShouldNotBeSatisfied(
+			string matchingPattern)
+		{
+			Type type = typeof(ResideInNamespaceTests);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldNotResideInNamespace(matchingPattern);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldBeViolated();
+			result.Errors[0].Should().BeOfType<TypeTestError>()
+				.Which.Type.Should().Be(type);
+			result.Errors[0].Should().BeOfType<TypeTestError>()
+				.Which.ToString().Should().Contain($"'{matchingPattern}'");
+		}
+
+		[Theory]
+		[InlineData("*Foo")]
+		[InlineData("Test??.Architecture.Rules.Tests.Requirements")]
+		[InlineData("TESTABLY.architecture.RULES.tests.REQUIREMENTS")]
+		public void ShouldNotResideInNamespace_NotMatchingPattern_ShouldNotBeViolated(
+			string notMatchingPattern)
+		{
+			Type type = typeof(ResideInNamespaceTests);
+			IRule rule = Expect.That.Types
+				.WhichAre(type)
+				.ShouldNotResideInNamespace(notMatchingPattern);
+
+			ITestResult result = rule.Check
+				.InAllLoadedAssemblies();
+
+			result.ShouldNotBeViolated();
+		}
+	}
+}
